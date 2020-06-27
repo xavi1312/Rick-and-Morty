36baa84d794ceb2df5a79f3d85f1cdec9d6a283b
Xavi <xavierisachalemany@gmail.com>
2020-06-28 00:22:26 +0200

    Create a hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,23 @@
 import React from "react";
 // Assets
+import "../assets/styles/components/Hero.scss";
+import heroImg from "../assets/static/hero.png";
 
 const Hero = () => (
   <>
-    <div className="hero">
-      <h1>Rick and Morty</h1>
+    <div className="hero" style={{ backgroundImage: `url(${heroImg})` }}>
+      <div className="hero__content">
+        <h1 className="hero__title">Rick and Morty</h1>
+        <p className="hero__description">
+          Descubre tus personajes favoritos de la famosa seria Riack and Morty.
+          <br />
+          Aquí encontrarás sus nombres, su primera y última aparición y mucho
+          más.
+        </p>
+      </div>
+      <div id="mouse_body">
+        <div id="mouse_wheel"></div>
+      </div>
     </div>
   </>
 );
--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -2,6 +2,8 @@ import React from "react";
 // Utilities
 // Components
 import Hero from "../components/Hero";
+// Assets
+import "../assets/styles/App.scss";
 
 const Home = () => {
   return <Hero />;
