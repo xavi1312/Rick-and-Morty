4f7e25745af60adeff32bcf3b20d8e1ab16e4de1
Xavi <xavierisachalemany@gmail.com>
2020-07-01 20:38:15 +0200

    include logo if characters is empty

--- src/components/Cards-grid.jsx
+++ src/components/Cards-grid.jsx
@@ -1,9 +1,20 @@
-import React from "react";
+import React from 'react';
+//  Components
+import Logo from '../components/Logo';
 // Assets
-import "../assets/styles/components/Cards-grid.scss";
+import '../assets/styles/components/Cards-grid.scss';
 
 const CardsGrid = ({ children }) => {
-  return <main className="cards-grid">{children}</main>;
+  return (
+    <main
+      style={children.length === 0 ? { display: 'flex' } : null}
+      className='cards-grid'
+    >
+      {children.length === 0 ? <Logo width='4' /> : ''}
+
+      {children}
+    </main>
+  );
 };
 
 export default CardsGrid;
